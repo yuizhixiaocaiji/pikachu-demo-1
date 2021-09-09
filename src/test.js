ec7097c8d3d120c76f18bc833d81c823eb4dce54
你çxuCheng <1390683299@qq.com>
2021-09-09 11:06:43 +0800

    init

---
+++ src/test.js
@@ -0,0 +1,65 @@
+import string from './css.js'
+
+const player = {
+  id: undefined,
+  ui: {
+    demo: document.querySelector('#demo'),
+    demo2: document.querySelector('#demo2')
+  },
+  n: 1,
+  time: 100,
+  init: () => {
+    player.ui.demo.innerText = string.substr(0, player.n)
+    player.ui.demo2.innerHTML = string.substr(0, player.n)
+    player.bindEvents()
+    player.play()
+  },
+  events: {
+    '#btnPause': 'pause',
+    '#btnPlay': 'play',
+    '#btnSlow': 'slow',
+    '#btnNormal': 'normal',
+    '#btnFast': 'fast'
+  },
+  bindEvents: () => {
+    for (let key in player.events) {
+      const value = player.events[key]
+      document.querySelector(key).onclick = player[value]
+    }
+  },
+  run: () => {
+    player.n += 1
+    if (player.n > string.length) {
+      window.clearInterval(player.id)
+      return
+    }
+    player.ui.demo.innerText = string.substr(0, player.n)
+    player.ui.demo2.innerHTML = string.substr(0, player.n)
+    player.ui.demo.scrollTop = player.ui.demo.scrollHeight
+  },
+  play: () => {
+    player.id = setInterval(player.run, player.time)
+  },
+  pause: () => {
+    window.clearInterval(player.id)
+  },
+  slow: () => {
+    player.pause()
+    player.time = 300
+    player.play()
+  },
+  normal: () => {
+    player.pause()
+    player.time = 100
+    player.play()
+  },
+  fast: () => {
+    player.pause()
+    player.time = 0
+    player.play()
+  }
+}
+
+player.init()
+
+
